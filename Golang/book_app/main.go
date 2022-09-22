e904ba2170ff3b8bdec3d68b24d2097296de3710
R-Lau <raymondlau@Raymonds-MBP.localdomain>
2022-09-21 21:55:44 -0700

    Used slices and arrays. Used variables and added user input.

--- Golang/book_app/main.go
+++ Golang/book_app/main.go
@@ -5,12 +5,37 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
 
-	fmt.Println("Welcome to our", conferenceName, "Booking application!")
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "tickets are still available.")
+	fmt.Printf("Welcome to our %v Booking application!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here.")
 
+	var firstName string
+	var lastName string
+	var email string
+	var customerTickets int
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets to book: ")
+	fmt.Scan(&customerTickets)
+
+	remainingTickets = remainingTickets - uint(customerTickets)
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you, %v %v for booking %v tickets. The confirmation will be sent to %v\n", firstName, lastName, customerTickets, email)
+	fmt.Printf("The remaining tickets in the system is: %v\n", remainingTickets)
+
+	fmt.Printf("These are all of our bookings: %v\n", bookings)
 }
